167ad9171a27b0a4f2b8207f7ab6891f00679189
Oscar Lira <programador.junior@caborca.biz>
2019-08-05 15:47:54 -0500

    se agregó configuración en app

---
+++ app/components/AppConfig.vue
@@ -0,0 +1,51 @@
+<template>
+  <Page>
+    <ActionBar title="Configuración - UNUS" />
+    <StackLayout orientation="vertical" class="m-20">
+       <Label  class="" >*IP</Label>
+       <TextField v-model="config.ip"/>
+       <Label >*Puerto</Label> 
+       <TextField v-model="config.puerto"/>
+        <Button
+            :text="'fa-check'  | fonticon"
+            width="120"
+            horizontalAlignment="right"
+            class="fa btn-app btn btn-rounded-sm"
+          />
+    </StackLayout>
+  </Page>
+</template>
+
+<script >
+
+import { alert } from "tns-core-modules/ui/dialogs";
+import { setString, getString } from 'tns-core-modules/application-settings';
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+      config:{
+        ip:'',
+        port:''
+      }
+    };
+  },
+  computed: {
+    
+  },
+  mounted: function() {
+     this.config.ip = getString("IP","");
+     this.config.puerto = getString("PUERTO","");
+  },
+  methods: {
+    guardar(){
+      setString("IP", this.config.ip);
+      setString("PUERTO", this.config.puerto);
+    }
+  }
+};
+</script>
+
+
